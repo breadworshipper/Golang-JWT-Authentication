2ecfd9dc395b767f0dc63bfba5568e0637d4e0ef
breadworshipper <azhrayashna@gmail.com>
2024-08-29 14:36:08 +0700

    FEAT: User Controller

--- controllers/userController.go
+++ controllers/userController.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"authentication/database"
 	"authentication/helpers"
-	helper "authentication/helpers"
 	"authentication/models"
 	"context"
 	"fmt"
@@ -14,10 +13,12 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"golang.org/x/crypto/bcrypt"
 )
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var validator = validator.New()
 
 func HashPassword() {
 	
@@ -39,6 +40,23 @@ func GetUsers() {
 
 }
 
-func GetUser() {
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userId := c.Param("user_id")
 
+		if err := helper.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+			return 
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100 * time.Second)
+		defer cancel()
+
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, user)
+	}
 }
